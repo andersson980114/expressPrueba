d51a656924406ccd3b683d60830f0184a5844a8f
Marin28c1997 <>
2023-03-06 08:10:45 -0500

    Update pagina.js

--- Models/pagina.js
+++ Models/pagina.js
@@ -1,32 +1,30 @@
-const app = Vue.createApp({
-  setup() {
-    const nombre = Vue.ref('');
-    const descripcion = Vue.ref('');
-    const tarea = Vue.ref([]);
-
-    const agregarTareas = () => {
-      tarea.value.push({
-        nombre: nombre.value,
-        descripcion: descripcion.value,
-      });
-
-      localStorage.setItem('tareas', JSON.stringify(tarea.value));  
-      nombre.value = '';
-      descripcion.value = '';
-    };
-
-    const listaTareas = JSON.parse(localStorage.getItem('tareas'));
-    if (listaTareas !== null) {
-      tarea.value = listaTareas;
-    }
-
-    return {
-      nombre,
-      descripcion,
-      tarea,
-      agregarTareas,
-    };
+var app = new Vue({
+  el: "#app",
+  data: {
+    name: '',
+    description:'',
+    task: [],
   },
-});
 
-app.mount('#app');
+methods: {
+  addTask() {
+    this.task.push({
+      name: this.name,
+      description: this.description,
+    });
+
+    localStorage.setItem('tareas', JSON.stringify(this.task));  
+    this.name = '';
+    this.description= '';
+  },
+
+
+},
+mounted() {
+ 
+  const listTask = JSON.parse(localStorage.getItem("tareas"))
+  if (listTask!==null) {
+    this.task = listTask;
+  }
+}
+});
